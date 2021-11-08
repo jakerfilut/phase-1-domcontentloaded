85ea11d2372f9adb91985b345da80ca077550c0b
Jake <jakerfilut@gmail.com>
2021-11-08 14:54:08 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+   let change = document.getElementById("text").textContent = "This is really cool!"
+  });
+  
+  
+  
